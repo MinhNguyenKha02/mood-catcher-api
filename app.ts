d71a89df4ef30b4136469f5dbc34abd765f86249
jamesnguyen <jamenguyen.957@gmail.com>
2025-04-17 17:36:40 +0700

    wp: audio-transcribe-vosk-service implement

--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
 // we will start here, I call a main code flow
 
+import dotenv from 'dotenv';
+dotenv.config();
--- routes/audio.ts
+++ routes/audio.ts
@@ -1,3 +1,20 @@
 // I will build upload api here gate of mood catcher
+import fs from 'fs';
+import { Model, Recognizer } from 'vosk';
+import { Readable } from 'stream';
+
+const MODEL_PATH = process.env.MODEL_PATH;
+const SAMPLE_RATE =process.env.SAMPLE_RATE;
+if (!MODEL_PATH || !SAMPLE_RATE) {
+    throw new Error('MODEL_PATH is not defined in .env');
+}
+
+let model: Model;
+try {
+    model = new Model(MODEL_PATH);
+} catch (err) {
+    console.error('Failed to load Vosk model:', err);
+    process.exit(1);
+}
 
 
